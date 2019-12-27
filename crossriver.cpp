be634a6a85fb36290176bbe2c43297eb56183a59
Paraworker <46927151+Paraworker@users.noreply.github.com>
2019-12-27 23:04:55 +0800

    Add files via upload

--- crossriver.cpp
+++ crossriver.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include<QString>
 #include"crossriver.h"
--- introwindow.cpp
+++ introwindow.cpp
@@ -8,6 +8,8 @@ IntroWindow::IntroWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     this->ui->textBrowser->setPlainText(about());
+    this->ui->label_website->setText("<a style='color: #2952A0;' href=\"https://github.com/Paraworker/CrossRiver\">网站");
+    this->ui->label_licenses->setText("<a style='color: #2952A0;' href=\"https://www.gnu.org/licenses/gpl-3.0.html\">GNU 通用公共许可证 v3.0");
 }
 
 IntroWindow::~IntroWindow()
